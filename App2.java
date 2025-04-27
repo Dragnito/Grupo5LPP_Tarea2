a4f5a28ff21d64c1173d8fd5a1ca0d0afd38a494
Looffardo <christianlooffh@gmail.com>
2025-04-27 17:35:07 -0400

    Interfaz construida

--- App2.java
+++ App2.java
@@ -1,8 +1,140 @@
-import menu.Menu;import modelos.*;
-import servicios.*;
-import java.time.LocalDate;
-import java.util.List;
+import menu.Menu;
 
 public class App2 {
-    
+
+    public static void main(String[] args) {
+        // Crear el menú
+        Menu menu = new Menu();
+        boolean salir = false;
+
+        while (!salir) {
+            int opcionPrincipal = menu.mostrarMenuPrincipal();
+
+            switch (opcionPrincipal) {
+                case 1:
+                    gestionarCultivos(menu);
+                    break;
+                case 2:
+                    gestionarParcelas(menu);
+                    break;
+                case 3:
+                    gestionarActividades(menu);
+                    break;
+                case 4:
+                    gestionarBusquedaReporte(menu);
+                    break;
+                case 5:
+                    System.out.println("¡Gracias por usar el sistema! ¡Adiós!");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+            }
+        }
+    }
+
+    private static void gestionarCultivos(Menu menu) {
+        boolean volver = false;
+        while (!volver) {
+            int opcion = menu.mostrarMenuCultivos();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Listar cultivos");
+                    // Aquí iría el llamado a la función real de listar cultivos
+                    break;
+                case 2:
+                    System.out.println("Crear cultivo");
+                    // Función de crear cultivo
+                    break;
+                case 3:
+                    System.out.println("Editar cultivo");
+                    // Función de editar cultivo
+                    break;
+                case 4:
+                    System.out.println("Eliminar cultivo");
+                    // Función de eliminar cultivo
+                    break;
+                case 5:
+                    volver = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+            }
+        }
+    }
+
+    private static void gestionarParcelas(Menu menu) {
+        boolean volver = false;
+        while (!volver) {
+            int opcion = menu.mostrarMenuParcelas();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Listar parcelas");
+                    break;
+                case 2:
+                    System.out.println("Crear parcela");
+                    break;
+                case 3:
+                    System.out.println("Editar parcela");
+                    break;
+                case 4:
+                    System.out.println("Eliminar parcela");
+                    break;
+                case 5:
+                    System.out.println("Asignar cultivo a parcela");
+                    break;
+                case 6:
+                    volver = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+            }
+        }
+    }
+
+    private static void gestionarActividades(Menu menu) {
+        boolean volver = false;
+        while (!volver) {
+            int opcion = menu.mostrarMenuActividades();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Registrar actividad");
+                    break;
+                case 2:
+                    System.out.println("Listar actividades de un cultivo");
+                    break;
+                case 3:
+                    System.out.println("Eliminar actividad");
+                    break;
+                case 4:
+                    System.out.println("Marcar actividad como completada");
+                    break;
+                case 5:
+                    volver = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+            }
+        }
+    }
+
+    private static void gestionarBusquedaReporte(Menu menu) {
+        boolean volver = false;
+        while (!volver) {
+            int opcion = menu.mostrarMenuBusquedaReporte();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Buscar cultivos por nombre o variedad");
+                    break;
+                case 2:
+                    System.out.println("Reporte de cultivos");
+                    break;
+                case 3:
+                    volver = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+            }
+        }
+    }
 }
