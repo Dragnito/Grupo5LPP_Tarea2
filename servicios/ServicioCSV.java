aae42262d6404b6bc1e73fed93c7ec3f9a1a1cd1
NotGormi <70240779+NotGormi@users.noreply.github.com>
2025-04-29 21:27:59 -0400

    Implementacion del lector de CSV
    el codigo ta maomeno en su esqueleto, falta ver la implementacion completa segun los parametros del resto del workspace

--- servicios/ServicioCSV.java
+++ servicios/ServicioCSV.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 package servicios;
 
 public class ServicioCSV {
-    
+
     // Importante
 
     // El archivo CSV se pasa como argumento al programa (args[0])
+
+    private BufferedReader lector; // Lee el archivo
+    private String linea; // Recibe la linea de cada fila
+    private String partes[] = null; // Almacena cada linea leída
+
+    public void leerArchivo(String nombreArchivo){
+        try {
+            lector = new BufferedReader(new FileReader(nombreArchivo));
+            while ((linea = lector.readLine()) != null) {
+                partes = linea.split(",");
+                
+            }
+        }
+    }
 
 }
